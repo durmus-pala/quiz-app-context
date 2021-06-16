837da754fa77698db7d22586e9b1a288013a36c3
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-06-16 13:48:53 +0300

    add questions to modal

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Modal = () => {
-  const { isModalOpen, closeModal, correct } = useGlobalContext();
+  const { isModalOpen, closeModal, correct, questions } = useGlobalContext();
   return (
     <div
       className={`${
